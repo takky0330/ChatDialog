78a7eae344190982b15404d8f17d38c731ee30d7
Shin Takizawa <takizawa@YRL47-52560.local>
2020-07-04 21:20:12 +0900

    Xsmall

--- config.py
+++ config.py
@@ -26,8 +26,8 @@ class Config:
     data_dir = './data'
     train_data_path = f'{data_dir}/train_data.txt'
     #pickle_path = f'{data_dir}/train_data.pkl'
-    pickle_path = f'{data_dir}/train_data_small.pkl'  ### ← これで、既存のTrainデータを利用
-    fn = 'ckpt_small'
+    pickle_path = f'{data_dir}/train_data_Xsmall.pkl'  ### ← これで、既存のTrainデータを利用
+    fn = 'ckpt_Xsmall'
 
     load = False
     # FIXME: if you use original data, change flag of this
