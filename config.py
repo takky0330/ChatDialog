ae0a9133959161f8f57b155e641deb561cf17aa6
Shin Takizawa <takizawa@YRL47-52560.local>
2020-07-06 10:37:23 +0900

    Large

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ class Config:
     train_data_path = f'{data_dir}/train_data.txt'
     #pickle_path = f'{data_dir}/train_data.pkl'
     pickle_path = f'{data_dir}/train_data.pkl'  ### ← これで、既存のTrainデータを利用
-    fn = 'ckpt_full_gpu'
+    fn = 'ckpt_large_gpu'
 
     load = False
     # FIXME: if you use original data, change flag of this
